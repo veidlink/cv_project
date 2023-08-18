0ff37cea2c636e71bbf8faff1836c0aa6e8c305d
Vlad1slawoo <phvlad02@mail.ru>
2023-08-18 10:43:30 +0300

    hot fix

--- pages/Denoising_Autoencoding.py
+++ pages/Denoising_Autoencoding.py
@@ -1,37 +1,22 @@
-import torch
-import torch.nn as nn
 import streamlit as st
 import torch
-import matplotlib.pyplot as plt
-
-from PIL import Image
-from torchvision import transforms
 from AutoEncoderClass import ImprovedAutoencoderNoPooling
 from AutoEncoderClass import process_image
-
 
 st.set_page_config(
     page_title='Очищение документов от шумов с помощью автоэнкодера',  # Setting page title
     page_icon="🤖"     # Setting page icon
 )
-
 st.write('## Очищение документов от шумов с помощью автоэнкодера')
-
 device = 'cpu'
 model = torch.load('weights/AutoEncoderWeights.pth', map_location=device)
 model.eval()
-
 uploaded_file = st.file_uploader("##### Выберите файл...")
-
-
 col1, col2 = st.columns(2)
 
 if uploaded_file:
-    
     input_image, output_image = process_image(uploaded_file, model)
-
     with col1:
-
         try:
             st.image(uploaded_file,
                     caption="Загруженное фото",
@@ -39,7 +24,6 @@ if uploaded_file:
         except Exception as ex:
             st.error(
                 f"Загрузите фото.")
-
     with col2:
         try:
             st.image(output_image, caption='Фото без шума', use_column_width=True)  
--- pages/Pizza_Ingridients.py
+++ pages/Pizza_Ingridients.py
@@ -1,16 +1,7 @@
 import torch
 import PIL
 import streamlit as st
-import matplotlib.pyplot as plt
-import requests
-
-from torchvision import transforms as T
 from PIL import Image
-from io import BytesIO
-
-# Model
-
-# model_path = 'weights/FoodIngredientsWeights.pt'
 
 st.set_page_config(
     page_title='Детекция ингридиентов пиццы с помощью YOLOv5',  # Setting page title
@@ -25,61 +16,29 @@ model = torch.hub.load(
 path='weights/FoodIngredientsWeights.pt', # путь к нашим весам
 source='local' # откуда берем модель – наша локальная
 )
-
 model.conf = st.select_slider('Установите порог доверия модели:',
                               options=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
 
-
-# # Function to download and display the image
-# def save_image_from_link(url):
-#     try:
-#         response = requests.get(url)
-#         source_img = Image.open(BytesIO(response.content))
-#     except Exception as e:
-#         st.error(f"Error: {e}")
-#     return source_img
-
-
 source_img = st.file_uploader(
         "##### Выберите файл")
-
-# # User input for image URL
-# url = st.text_input("Или предоставьте ссылку на него")
-
-# source_img = save_image_from_link(url)
-
 col1, col2 = st.columns(2)
-
 if source_img:
-        
     uploaded_image = PIL.Image.open(source_img)
-
     with col1:
-    
-
         try:
-
             st.image(source_img,
                     caption="Загруженное фото",
                     )
-
-            # model.load_state_dict(torch.load('/home/veidlink/ds_bootcamp/CVisionProject/web_page/weights/FoodIngredientsWeights.pt')['model'].state_dict())
         except Exception as ex:
             st.error(
                 f"Загрузите фото.")
-
     with col2:
-
         try:
-
             result = model(uploaded_image)
-            
             result = result.render()
-            
             st.image(result,
                     caption="Фото с детекцией",
                     )
-
         except Exception as ex:
             st.error(
                 "Невозможно провести детекцию.")
